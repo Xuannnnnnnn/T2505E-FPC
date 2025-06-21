918fef3bcabd46b4248d9c5e78d72abeff2b4d8a
Xuannnnnnnn <85539721+Xuannnnnnnn@users.noreply.github.com>
2025-06-21 15:54:52 +0700

    BTVN_B9

---
+++ B9/Bai1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include "khaibaoham.h"
+
+int main(){	
+	//menu();
+	int z = total(4,7);	
+	int x = chuvi(3,4,5);
+	int y = dientich(3,4,5);
+	int m = tinhtong(2345);
+	//printf("%d",m);
+	int n = UCLN(13,6);
+	printf("UCLN: %d\n",n);
+	
+	int q = BCNN(13,29);
+	printf("BCNN: %d\n",q);
+	
+	int k = luythua(3,3);
+	printf("Luy thua: %d",k);
+	
+}
+
+
